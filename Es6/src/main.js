dd506ed4d78bb5057bde3b0fe6eb7a2ed86f7913
ahmedfouad <lionmoon60@yahoo.com>
2018-09-25 22:53:40 +0200

    template literal ES6

--- Es6/src/main.js
+++ Es6/src/main.js
@@ -1,48 +1,82 @@
 'use strict'
-class User{
-  constructor(username,email,password){
-    this.username=username;
-    this.email=email;
-    this.password=password;
-  }
-  static countUsers(){
-    console.log("you can call me without initation of the class");
-  }
+// class User{
+//   constructor(username,email,password){
+//     this.username=username;
+//     this.email=email;
+//     this.password=password;
+//   }
+//   static countUsers(){
+//     console.log("you can call me without initation of the class");
+//   }
 
 
-  register(){
-    console.log(this.username +' is now registered');
-  }
+//   register(){
+//     console.log(this.username +' is now registered');
+//   }
    
+// }
+
+// /** we can inherit from another class */
+
+// class Member extends User{
+//   constructor(username,email,password,memberPackage){
+//     super(username,email,password);
+//     this.memberPackage=memberPackage;
+//   }
+//   getPackage(){
+//     console.log(this.username+' is subscriped to '+this.memberPackage+" package")
+//   }
+// }
+
+
+
+
+
+// /** Main */
+
+// let foush=new User("foush","foush@foush.com","123");
+
+// foush.register();
+
+// /** for static methods in the class it can be called without initation */
+// User.countUsers();
+
+
+// /** extending from another class */
+// let m1=new Member("foush","foush@foush.com","123","Standard");
+
+// m1.getPackage();
+
+let name="foush"
+
+function makeUpperCase(word){
+  return word.toUpperCase();
 }
+let template=`<h1>${makeUpperCase('hello')} ,${name}</h1>
+<p>this is a simple template</p>`;
 
-/** we can inherit from another class */
-
-class Member extends User{
-  constructor(username,email,password,memberPackage){
-    super(username,email,password);
-    this.memberPackage=memberPackage;
-  }
-  getPackage(){
-    console.log(this.username+' is subscriped to '+this.memberPackage+" package")
-  }
-}
+document.querySelector('#template').innerHTML = template;
 
 
 
 
 
-/** Main */
-
-let foush=new User("foush","foush@foush.com","123");
-
-foush.register();
-
-/** for static methods in the class it can be called without initation */
-User.countUsers();
 
 
-/** extending from another class */
-let m1=new Member("foush","foush@foush.com","123","Standard");
 
-m1.getPackage();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
