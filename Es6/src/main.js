b213aa8e804f6890395882a7a6b9864e55e7b235
ahmedfouad <lionmoon60@yahoo.com>
2018-09-25 22:30:30 +0200

    classes in Es6

--- Es6/src/main.js
+++ Es6/src/main.js
@@ -5,12 +5,44 @@ class User{
     this.email=email;
     this.password=password;
   }
+  static countUsers(){
+    console.log("you can call me without initation of the class");
+  }
+
+
   register(){
     console.log(this.username +' is now registered');
   }
    
 }
 
+/** we can inherit from another class */
+
+class Member extends User{
+  constructor(username,email,password,memberPackage){
+    super(username,email,password);
+    this.memberPackage=memberPackage;
+  }
+  getPackage(){
+    console.log(this.username+' is subscriped to '+this.memberPackage+" package")
+  }
+}
+
+
+
+
+
+/** Main */
+
 let foush=new User("foush","foush@foush.com","123");
 
 foush.register();
+
+/** for static methods in the class it can be called without initation */
+User.countUsers();
+
+
+/** extending from another class */
+let m1=new Member("foush","foush@foush.com","123","Standard");
+
+m1.getPackage();
